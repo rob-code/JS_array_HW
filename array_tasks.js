6952d40f47616fd527a345b4fb7ca7a6e62205ee
Robert Brice <rob.a.brice@gmail.comC>
2017-04-05 18:15:09 +0100

    passing test findIndexOf

--- array_tasks.js
+++ array_tasks.js
@@ -4,29 +4,64 @@ var arrayTasks = {
 		return arr1.concat(arr2);
 	},
 
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
 
-	// square: function (arr) {
-		
-	// },
+	square: function (arr) {
+		var resultArray = [];
+		arr.forEach(function(item){
+				resultArray.push(item * item)
+		})
+		return resultArray;
+	},
 
-	// sum: function (arr) {
-		
-	// },
+	sum: function (arr) {
+		var sum = 0;
+		arr.forEach(function(item){
+			sum += item
+		})
+			return sum
+	},
 
-	// findDuplicates: function (arr) {
-		
-	// },
+	findDuplicates: function (arr) {
+		var resultArray = [];
+			for (var i of arr){
+			if (arr[i+1] == arr[i]){
+				resultArray.push(arr[i]);
+				}
+			}
 
-	// removeAndClone: function (arr, valueToRemove) {
-		
-	// },
+			for (var i = 0; i < resultArray.length; i++){
+				if (resultArray[i+1] == resultArray[i]){
+						resultArray.splice(i+1);		
+				}
+			}
+			return resultArray
+	},
 
-	// findIndexesOf: function (arr, itemToFind) {
-		
-	// },
+	removeAndClone: function (arr, valueToRemove) {
+			var result = new Array
+			for (var i = 0; i < arr.length; i++){
+				if (arr[i] != valueToRemove){
+					result.push(arr[i]);
+					}
+			}
+			return result
+	},
+
+	findIndexesOf: function (arr, itemToFind) {
+		var result = new Array
+		for (var i = 0; i < arr.length; i++){
+			if (arr[i] == itemToFind){
+				result.push(i);
+				}
+		}
+		return result
+
+
+	},
 
 	// sumOfAllEvenNumbersSquared: function (arr) {
 		
